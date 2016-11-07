efd4d7864fb8100b9eb040885846181987a22662
Daniel Ornelas <danielo@zwift.com>
2016-11-07 15:33:43 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Zwift Services
+Plugin URI: http://zwift.com/
+Description: WordPress Plug-in
+Version: 1.0
+Author: Zwift.com
+Author URI: http://zwift.com/
+Network: true
+*/
+
+require_once "/var/www/vendor/autoload.php";
+
+new Zwift\WP\Services;
+new Zwift\WP\Invites;
